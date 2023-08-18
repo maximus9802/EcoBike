b83aedaa661a6495adc1a59ef2b2711e43e24a0a
coderXomDuong5 <Thckpck11@gmail.com>
2023-08-19 00:18:00 +0700

    API get and check card information

--- backend/src/main/java/com/quyvx/ecobike/domain/aggregate_models/Card.java
+++ backend/src/main/java/com/quyvx/ecobike/domain/aggregate_models/Card.java
@@ -1,16 +1,16 @@
 package com.quyvx.ecobike.domain.aggregate_models;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.UUID;
 
 @Getter
 @Setter
 @SuperBuilder
 @Slf4j
+@NoArgsConstructor
 public class Card {
     private String cardCode;
     private String cardHolderName;
